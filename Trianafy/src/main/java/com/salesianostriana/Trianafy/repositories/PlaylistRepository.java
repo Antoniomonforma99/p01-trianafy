9951d1ab60ee5da7e9b6b7258f81a5fe11063694
alejandrosalesianos <martin.cuale21@triana.salesianos.edu>
2021-10-05 12:36:57 +0200

    fixed PlaylistRepository

--- Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/PlaylistRepository.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/PlaylistRepository.java
@@ -3,5 +3,5 @@ package com.salesianostriana.Trianafy.repositories;
 import com.salesianostriana.Trianafy.models.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaylistRepositoy extends JpaRepository<Playlist,Long> {
+public interface PlaylistRepository extends JpaRepository<Playlist,Long> {
 }

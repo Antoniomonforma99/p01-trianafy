a5175ba8aa1aff67388c72da90d529de0e46d0c8
Guillermo De La Cruz Guzmán <delacruz.gugui20@triana.salesianos.edu>
2021-10-05 12:40:28 +0200

    Modifico el metodo GET de un artista

--- Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/ArtistController.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/ArtistController.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.Trianafy.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,4 +16,15 @@ public class ArtistController {
 
 
     @GetMapping("/{id}")
-    public Artist findOne (@PathVariable("id") Long id ){ return repository.findById(id).orElse(null); }
+    public ResponseEntity<Artist> findOne (@PathVariable("id") Long id ){
+        return ResponseEntity
+                .of(repository.findById(id));
+    }
+
+
+
+
+
+
+
+}

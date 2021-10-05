2e4f9d215d5bb18e8bd203ff2220e46b80dd134c
Guillermo De La Cruz Guzmán <delacruz.gugui20@triana.salesianos.edu>
2021-10-05 12:14:24 +0200

    Create RepositoryArtist

---
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/RepositoryArtist.java
@@ -0,0 +1,6 @@
+package com.salesianostriana.Trianafy.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryArtist extends JpaRepository<Artist, Long> {
+}

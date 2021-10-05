c703c95fa6beccafb794f8a905bd80ff22ff996e
Guillermo De La Cruz Guzmán <delacruz.gugui20@triana.salesianos.edu>
2021-10-05 14:04:50 +0200

    Modificación extensión interface SongRepository

--- Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/SongRepository.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/SongRepository.java
@@ -4,5 +4,5 @@ package com.salesianostriana.Trianafy.repositories;
 import com.salesianostriana.Trianafy.models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SongRepository extends JpaRepository<Long, Song> {
+public interface SongRepository extends JpaRepository<Song, Long> {
 }

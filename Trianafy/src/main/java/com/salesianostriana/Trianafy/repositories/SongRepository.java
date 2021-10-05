d6ee53d586177d587ab6bf31b626bbc569a893a2
antonio Montero <antoniomonforma@gmail.com>
2021-10-05 12:15:58 +0200

    Song repository created

---
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/repositories/SongRepository.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.Trianafy.repositories;
+
+
+import com.salesianostriana.Trianafy.models.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SongRepository extends JpaRepository<Long, Song> {
+}

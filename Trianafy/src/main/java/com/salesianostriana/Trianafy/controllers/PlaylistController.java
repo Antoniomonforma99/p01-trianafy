e0d7362b195844ccaf17a1af0e49e902b80cb272
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-10-05 22:18:44 +0200

    Añadido metodo POST en PlaylistController para agregar canciones a Playlist

--- Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/PlaylistController.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/PlaylistController.java
@@ -1,22 +1,26 @@
 package com.salesianostriana.Trianafy.controllers;
 
+import com.salesianostriana.Trianafy.DTOs.CreateSongDto;
+import com.salesianostriana.Trianafy.DTOs.PlaylistDtoConverter;
+import com.salesianostriana.Trianafy.DTOs.SongDtoConverter;
 import com.salesianostriana.Trianafy.models.Playlist;
+import com.salesianostriana.Trianafy.models.Song;
 import com.salesianostriana.Trianafy.repositories.PlaylistRepository;
+import com.salesianostriana.Trianafy.repositories.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/playlist")
+@RequestMapping("/list")
 public class PlaylistController {
 
     private final PlaylistRepository repository;
+    private final SongRepository SongRepository;
 
 
     @GetMapping("/{id}")
@@ -29,4 +33,34 @@ public class PlaylistController {
     }
 
 
+    @PostMapping("/{id1}/songs/{id2}")
+    public ResponseEntity<Playlist> addSong (@RequestBody Playlist p,
+                                             @PathVariable("id") Long id1,
+                                             @PathVariable("id") Long id2){
+
+        if ( (repository.findById(id2) == null)
+                &&( SongRepository.getById(id2) == null)){
+
+            return ResponseEntity.badRequest().build();
+
+        }else{
+
+            Song song = SongRepository.getById(id2);
+            p.getSongs().add(song);
+
+            return ResponseEntity.of(
+                    repository.findById(id1).map(m -> {
+                        m.setName(p.getName());
+                        m.setDescription(p.getDescription());
+                        m.setSongs(p.getSongs());
+                        repository.save(m);
+                        return m;
+                    })
+            );
+        }
+    }
+
+
+
+    
 }

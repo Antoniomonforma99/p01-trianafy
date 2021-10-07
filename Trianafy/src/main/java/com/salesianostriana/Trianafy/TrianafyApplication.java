530083dc1f5ca08c338a670b92e4ad892f30fd2c
alejandrosalesianos <alejandromartin2013@live.com>
2021-10-07 20:32:09 +0200

    Added OpenApi documentation

--- Trianafy/src/main/java/com/salesianostriana/Trianafy/TrianafyApplication.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/TrianafyApplication.java
@@ -1,9 +1,17 @@
 package com.salesianostriana.Trianafy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(description = "Proyecto 01 de AD y PSP Trianafy," +
+		" Alejandro Martín, Guillermo De la cruz, Antonio Montero",
+		version = "1.0",
+		title = "Trianafy")
+)
+
 public class TrianafyApplication {
 
 	public static void main(String[] args) {
--- Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/SongController.java
+++ Trianafy/src/main/java/com/salesianostriana/Trianafy/controllers/SongController.java
@@ -7,6 +7,11 @@ import com.salesianostriana.Trianafy.models.Artist;
 import com.salesianostriana.Trianafy.models.Song;
 import com.salesianostriana.Trianafy.repositories.ArtistRepository;
 import com.salesianostriana.Trianafy.repositories.SongRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.apache.bcel.Repository;
 import org.springframework.http.HttpStatus;
@@ -26,6 +31,13 @@ public class SongController {
     private final SongDtoConverter dtoConverter;
     private final ArtistRepository artistRepository;
 
+    @Operation(summary = "Mostrar todas las canciones de la base de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Devuelve todas las canciones",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Song.class)) }),
+            @ApiResponse(responseCode = "404", description = "Canciones no encontradas",
+                    content = @Content) })
     @GetMapping("/")
     public ResponseEntity<List<GetSongDto>> findAll() {
         List<Song> canciones = repository.findAll();
@@ -41,8 +53,15 @@ public class SongController {
         }
     }
 
-  
 
+    @Operation(summary = "Mostrar una canción por la id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Se muestra la canción",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Song.class))}),
+            @ApiResponse(responseCode = "404", description = "Si no se encuentra manda un notFound",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Song> findOne (@PathVariable("id") Long id) {
         if (repository.findById(id) == null) {
@@ -53,6 +72,14 @@ public class SongController {
             return ResponseEntity.of(repository.findById(id));
         }
     }
+    @Operation(summary = "Crear una cancion")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Se crea la canción",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Song.class))}),
+            @ApiResponse(responseCode = "400", description = "Si se introduce mal un dato manda BadRequest",
+                    content = @Content)
+    })
     @PostMapping("/")
         public ResponseEntity<Song> create (@RequestBody CreateSongDto dto){
 
@@ -67,6 +94,17 @@ public class SongController {
                     .status(HttpStatus.CREATED)
                     .body(repository.save(nueva));
         }
+    @Operation(summary = "Editar un canción")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Edita la canción",
+            content = {@Content(mediaType = "application/json",
+            schema = @Schema(implementation = Song.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Algún dato se envia mal",
+            content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encontra la id de la canción",
+            content = @Content)
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Song> edit (@RequestBody Song so, @PathVariable("id") Long id){
         return ResponseEntity.of(repository.findById(id).map(s -> {
@@ -79,6 +117,13 @@ public class SongController {
         );
     }
 
+    @Operation(summary = "Eliminar una canción")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Se elimina la canción",
+            content = @Content),
+            @ApiResponse(responseCode = "404", description = "No se encuentra la id de la canción",
+            content = @Content)
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Song> delete (@PathVariable("id") Long id) {
         if (repository.findById(id) == null){
